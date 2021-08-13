fab053d86e0562e7a661681a00b1f1b61a0965b4
Rainbow Cat <2056423011@qq.com>
2021-08-13 14:46:12 +0800

    Site updated: 2021-08-13 14:46:11

--- js/funny_title.js
+++ js/funny_title.js
@@ -1,1 +1,1 @@
-window.onload=function(){let t=document.title,e;document.addEventListener("visibilitychange",function(){document.hidden?(document.title="╭(°A°`)╮ 页面崩溃啦 ~",clearTimeout(e)):(document.title="(ฅ>ω<*ฅ) 噫又好了鸭~"+t,e=setTimeout(function(){document.title=t},4e3))})};
+$(function(){let t=document.title,e;document.addEventListener("visibilitychange",function(){document.hidden?(document.title="╭(°A°`)╮ 页面崩溃啦 ~",clearTimeout(e)):(document.title="(ฅ>ω<*ฅ) 噫又好了鸭~"+t,e=setTimeout(function(){document.title=t},4e3))})});
